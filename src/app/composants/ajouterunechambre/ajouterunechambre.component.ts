b33af576d923d14f4bc91114c0874c78100b625a
abou1er <abou.mlanao@gmail.com>
2022-06-07 09:10:33 +0200

    rectif add room ok

--- src/app/composants/ajouterunechambre/ajouterunechambre.component.ts
+++ src/app/composants/ajouterunechambre/ajouterunechambre.component.ts
@@ -10,20 +10,11 @@ export class AjouterunechambreComponent implements OnInit {
 
   constructor(private chambreService : ChambreService) { }
 
-  // constructor(private gestChambreService : GestChambreService) { }
-
   ngOnInit(): void {
   }
 
   addInRooms(chambresAdmin:any){
-    console.log("" + chambresAdmin.value);
-    let data = chambresAdmin.value
-    this.chambreService.addRoomAdmin(chambresAdmin).subscribe(data=>{
-
-     // this.gestChambreService.addRoomAdmin(chambresAdmin).subscribe(data=>{  si constructor = (private gestChambreService : GestChambreService)
-      console.log("données save");
-
-            
+    this.chambreService.addRoomAdmin(chambresAdmin.value).subscribe(data=>{          
     })
     
   }
